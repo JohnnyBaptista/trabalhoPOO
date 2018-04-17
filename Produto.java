9b07647a4e73965ed30782853fcba399662bb08f
Johnny Baptista <joaosbaptista@hotmail.com>
2018-04-17 19:56:12 -0300

    Classe do Produto v.01

---
+++ Produto.java
@@ -0,0 +1,25 @@
+public class Produto{
+	private String codigo, descricao;
+	private float valor;
+
+	public void setCodigo(String codigo){
+		this.codigo = codigo;
+	}
+	public String getCodigo(){
+		return this.codigo;
+	}
+	public void setDesc(String desc){
+		this.descricao = desc;
+	}
+	public String getDesc(){
+		return this.descricao;
+	}
+	public void setValor(float valor){
+		this.valor = valor;
+	}
+	public float getValor(){
+		return this.valor;
+	}
+
+	public abstract float calcularPrecos(); 
+}
