54f602e40f3c273111ce98592e28161e6f655ce5
Johnny Baptista <joaosbaptista@hotmail.com>
2018-04-17 19:46:54 -0300

    Classe do cliente v.01

---
+++ Cliente.java
@@ -0,0 +1,25 @@
+public class Cliente{
+	private String cpf;
+	private String nome;
+
+	public Cliente();
+
+	public void setCPF(String cpf){
+		this.cpf = cpf;
+	}
+
+	public String getCPF(){
+		return this.cpf;
+	}
+
+	public String getNome(){
+		return this.nome;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+
+
+}
